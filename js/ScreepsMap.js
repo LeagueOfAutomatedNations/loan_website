c1b835d5286f2efd1cbfff84772a4306658d27c5
Robert Hafner <tedivm@tedivm.com>
2016-10-01 23:57:59 -0700

    Add links from map to alliance member list, link to screeps itself in header

--- js/ScreepsMap.js
+++ js/ScreepsMap.js
@@ -252,8 +252,12 @@ ScreepsMap.prototype.drawColorKey = function() {
     let container = document.getElementById(this.colorKeyID);
     let output = '<ul class="colorKeyList">';
     for (let aName of this.allianceNames) {
+        output += '<div id=#colorkey_alliance_' + aName + '>'
         output += '<li class="colorKeyItem"><span class="colorBox" style="background-color: ' + this.colorForAlliance(aName) + ';"></span>';
+        output += '<a href="index.html#alliance_' + aName + '">'
         output += '<span class="colorLabel">' + this.alliances[aName].name + '</li>';
+        output += '</a>'
+        output += '</div>'
     }
     output += '</ul>';
     container.innerHTML = output;
